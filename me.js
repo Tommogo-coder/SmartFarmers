7c54ba9ea1eb4449ff5530a27c3769611593ccf4
Tommogo-coder <nelsontommogo9@gmail.com>
2023-03-08 20:08:25 +0300

    new commit

---
+++ me.js
@@ -0,0 +1,6 @@
+const submitButton = document.getElementById("submit");
+const signupButton = document.getElementById("sign-up");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const main = document.getElementById("main");
+const createacct = document.getElementById("create-acct");
